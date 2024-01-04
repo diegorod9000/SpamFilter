5d3c954577368cf4c41629c7f9d4b29fb49d745e
diegorod <diegorod@mit.edu>
2024-01-04 15:49:31 -0600

    more progress

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 import os
 
+DATA_PATH_CLEAN = "/data/clean"
+DATA_PATH_SPAM = "/data/spam"
+
 class Dict_Default(dict):
     """
     Identical to dict, except returns default instead of None if no key found.
@@ -23,3 +26,26 @@ def get_files_in_folder(folder):
     
     full_filenames = [os.path.join(folder, filename) for filename in filenames]
     return full_filenames
+
+def get_counts(file_list):
+    """
+    Given a list of files, counts total number of occurences of each word and 
+    outputs a dict containing the total for all files.
+    """
+    output = Dict_Default(0)
+    for file in file_list:
+        
+        words = set(get_all_words(file))
+        
+        for word in words:
+            output[word] +=1
+    
+    return output
+
+def get_words_clean():
+    fileList = get_files_in_folder(DATA_PATH_CLEAN)
+    return get_counts(fileList)
+
+def get_words_spam():
+    fileList = get_files_in_folder(DATA_PATH_SPAM)
+    return get_counts(fileList)
