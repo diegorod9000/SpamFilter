81440c03631cd7a96bf87cf15e270099e9fc9584
diegorod <diegorod@mit.edu>
2024-01-13 23:42:03 -0600

    made Pytorch Model

--- PyTorchModel.py
+++ PyTorchModel.py
@@ -1,19 +1,37 @@
 import numpy as np
 import tensorflow as tf
 import util
-import sklearn
 import torch
-from sklearn.neural_network import MLPClassifier
+import torch.nn as nn
+import torch.nn.functional as F
 
 DATA_PATH_TEST = "data/testing"  
-WEIGHT_CLEAN = 1
+WEIGHT_CLEAN = 2
 WEIGHT_SPAM = 1
+LEARNING_RATE = 0.01
+REGULARIZE = 1e-4
+NUM_EPOCHS = 10
+
+class Net(nn.Module):
+    def __init__(self, length):
+        super(Net, self).__init__()
+        # an affine operation: y = Wx + b
+        length1 = length>>1
+        length2 = length>>2
+        self.fc1 = nn.Linear(length, length1)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(length1, length2)
+        self.fc3 = nn.Linear(length2, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 def create_training_features():
     """
     Create training set of features and dictionary from clean and spam folders
     """
-    
     dictionary = util.get_dictionary()
     print("creating data dicts")
     train_clean = util.get_words_features_clean(dictionary)
@@ -32,21 +50,31 @@ def create_training_features():
             train_features.append(list(feature.values()))
             train_labels.append(1)
         
-    features_array = torch.tensor(train_features)
-    labels_array = torch.tensor(train_labels)
+    features = torch.tensor(train_features, dtype=torch.float32)
+    labels = torch.tensor(train_labels)
     
-    dataset = [features_array,labels_array]
-    return [dataset,dictionary]
+    
+    return features, labels, dictionary
 
 
-def train_model(dataset):
-    model = MLPClassifier()
+def train_network(features,labels):
+    
+    net = Net(len(features[0]))
     print("running training")
-    model.fit(dataset[0],dataset[1])
-    print("training complete")
-    return model
+    
+    optimizer = torch.optim.SGD(net.parameters(), lr=LEARNING_RATE, weight_decay=REGULARIZE)
+    criterion = nn.NLLLoss()
+    for i in range(NUM_EPOCHS):
+        optimizer.zero_grad()
+        output = net(features)
+        loss = criterion(output, labels)
+        print(loss)
+        loss.backward()
+        optimizer.step()
+    
+    return net
 
-def eval_model(model,dictionary):
+def eval_network(net,dictionary):
     
     files = util.get_files_in_folder(DATA_PATH_TEST)
     true_vals = []
@@ -70,15 +98,17 @@ def eval_model(model,dictionary):
             if word in nextFeature:
                 nextFeature[word]+=1
         test_features.append(list(nextFeature.values()))
-        
+    test_features = torch.Tensor(test_features)
+    
     print("predicting")
-    predictions = model.predict(test_features)
+    predictions = net(test_features)
     
     correctSpam = 0
-    correctClean =0
+    correctClean = 0
     for i in range(len(predictions)):
-        if predictions[i]==true_vals[i]:
-            if predictions[i] == 0:
+        prediction = 0 if predictions[i][0]>predictions[i][1] else 1
+        if prediction==true_vals[i]:
+            if prediction == 0:
                 correctClean+=1
             else:
                 correctSpam+=1
@@ -88,6 +118,8 @@ def eval_model(model,dictionary):
     
     
 if __name__ == "__main__":
-    dataset,dictionary = create_training_features()
-    model = train_model(dataset)
-    eval_model(model,dictionary)
+    
+    features, labels, dictionary = create_training_features()
+    model = train_network(features,labels)
+    eval_network(model,dictionary)
+    
